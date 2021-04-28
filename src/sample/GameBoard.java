c61ab33ff8c91935b0439c784258bc232f636d02
Autumn <asmith149@missouriwestern.edu>
2021-04-28 02:58:02 -0500

    Got basic game working

--- src/sample/GameBoard.java
+++ src/sample/GameBoard.java
@@ -1,15 +1,16 @@
 package sample;
 
-import javafx.beans.property.ReadOnlyDoubleProperty;
+
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -17,6 +18,7 @@ import java.io.InputStream;
 
 public class GameBoard extends BorderPane {
     private Scene gameScene;
+    private static int turns = 0;
 
     public GameBoard() throws FileNotFoundException {
 
@@ -28,98 +30,214 @@ public class GameBoard extends BorderPane {
 
         gameScene = new Scene(testGrid, 300, 275);
 
-        // First column of boxes
-        Rectangle r1 = new Rectangle();
-        r1.setFill(null);
-        r1.setStroke(Color.BLACK);
-        r1.heightProperty().bind(gameScene.heightProperty().divide(3.3));
-        r1.widthProperty().bind(gameScene.widthProperty().divide(3.3));
-
-        Rectangle r2 = new Rectangle();
-        r2.setFill(null);
-        r2.setStroke(Color.BLACK);
-        r2.heightProperty().bind(gameScene.heightProperty().divide(3.3));
-        r2.widthProperty().bind(gameScene.widthProperty().divide(3.3));
-
-        Rectangle r3 = new Rectangle();
-        r3.setFill(null);
-        r3.setStroke(Color.BLACK);
-        r3.heightProperty().bind(gameScene.heightProperty().divide(3.3));
-        r3.widthProperty().bind(gameScene.widthProperty().divide(3.3));
-
-        // second column of boxes
-        Rectangle r4 = new Rectangle();
-        r4.setFill(null);
-        r4.setStroke(Color.BLACK);
-        r4.heightProperty().bind(gameScene.heightProperty().divide(3.3));
-        r4.widthProperty().bind(gameScene.widthProperty().divide(3.3));
-
-        Rectangle r5 = new Rectangle();
-        r5.setFill(null);
-        r5.setStroke(Color.BLACK);
-        r5.heightProperty().bind(gameScene.heightProperty().divide(3.3));
-        r5.widthProperty().bind(gameScene.widthProperty().divide(3.3));
-
-        Rectangle r6 = new Rectangle();
-        r6.setFill(null);
-        r6.setStroke(Color.BLACK);
-        r6.heightProperty().bind(gameScene.heightProperty().divide(3.3));
-        r6.widthProperty().bind(gameScene.widthProperty().divide(3.3));
-
-        // third column of boxes
-        Rectangle r7 = new Rectangle();
-        r7.setFill(null);
-        r7.setStroke(Color.BLACK);
-        r7.heightProperty().bind(gameScene.heightProperty().divide(3.3));
-        r7.widthProperty().bind(gameScene.widthProperty().divide(3.3));
-
-        Rectangle r8 = new Rectangle();
-        r8.setFill(null);
-        r8.setStroke(Color.BLACK);
-        r8.heightProperty().bind(gameScene.heightProperty().divide(3.3));
-        r8.widthProperty().bind(gameScene.widthProperty().divide(3.3));
-
-        Rectangle r9 = new Rectangle();
-        r9.setFill(null);
-        r9.setStroke(Color.BLACK);
-        r9.heightProperty().bind(gameScene.heightProperty().divide(3.3));
-        r9.widthProperty().bind(gameScene.widthProperty().divide(3.3));
-
-        testGrid.add(r1, 0,0);
-        testGrid.add(r2, 0,1);
-        testGrid.add(r3, 0,2);
-        testGrid.add(r4, 1,0);
-        testGrid.add(r5, 1,1);
-        testGrid.add(r6, 1,2);
-        testGrid.add(r7, 2,0);
-        testGrid.add(r8, 2,1);
-        testGrid.add(r9, 2,2);
-
-
         //image stuff
         InputStream X = new FileInputStream("resources/X.png");
+        InputStream blankStream = new FileInputStream("resources/blank.png");
+        InputStream O = new FileInputStream("resources/o.png");
+
+        Image blank = new Image(blankStream);
         Image xImage = new Image(X);
+        Image oImage = new Image(O);
+
+        //first column
         ImageView imageView = new ImageView();
-        imageView.setImage(xImage);
+        imageView.setImage(blank);
         imageView.fitWidthProperty().bind(gameScene.widthProperty().divide(3.3));
         imageView.fitHeightProperty().bind(gameScene.heightProperty().divide(3.3));
         Group root = new Group(imageView);
         testGrid.add(root, 0, 0);
 
-        InputStream O = new FileInputStream("resources/o.png");
-        Image oImage = new Image(O);
+
         ImageView imageView2 = new ImageView();
-        imageView2.setImage(oImage);
+        imageView2.setImage(blank);
         imageView2.fitWidthProperty().bind(gameScene.widthProperty().divide(3.3));
         imageView2.fitHeightProperty().bind(gameScene.heightProperty().divide(3.3));
         Group root2 = new Group(imageView2);
-        testGrid.add(root2, 1, 2);
+        testGrid.add(root2, 0, 1);
+
+        ImageView imageView3 = new ImageView();
+        imageView3.setImage(blank);
+        imageView3.fitWidthProperty().bind(gameScene.widthProperty().divide(3.3));
+        imageView3.fitHeightProperty().bind(gameScene.heightProperty().divide(3.3));
+        Group root3 = new Group(imageView3);
+        testGrid.add(root3, 0, 2);
 
 
-        Button b1 = new Button();
-        b1.setStyle("-fx-border-color: transparent ");
+        //second column
+        ImageView imageView4 = new ImageView();
+        imageView4.setImage(blank);
+        imageView4.fitWidthProperty().bind(gameScene.widthProperty().divide(3.3));
+        imageView4.fitHeightProperty().bind(gameScene.heightProperty().divide(3.3));
+        Group root4 = new Group(imageView4);
+        testGrid.add(root4, 1, 0);
 
 
+        ImageView imageView5 = new ImageView();
+        imageView5.setImage(blank);
+        imageView5.fitWidthProperty().bind(gameScene.widthProperty().divide(3.3));
+        imageView5.fitHeightProperty().bind(gameScene.heightProperty().divide(3.3));
+        Group root5 = new Group(imageView5);
+        testGrid.add(root5, 1, 1);
+
+        ImageView imageView6 = new ImageView();
+        imageView6.setImage(blank);
+        imageView6.fitWidthProperty().bind(gameScene.widthProperty().divide(3.3));
+        imageView6.fitHeightProperty().bind(gameScene.heightProperty().divide(3.3));
+        Group root6 = new Group(imageView6);
+        testGrid.add(root6, 1, 2);
+
+
+        //third column
+        ImageView imageView7 = new ImageView();
+        imageView7.setImage(blank);
+        imageView7.fitWidthProperty().bind(gameScene.widthProperty().divide(3.3));
+        imageView7.fitHeightProperty().bind(gameScene.heightProperty().divide(3.3));
+        Group root7 = new Group(imageView7);
+        testGrid.add(root7, 2, 0);
+
+
+        ImageView imageView8 = new ImageView();
+        imageView8.setImage(blank);
+        imageView8.fitWidthProperty().bind(gameScene.widthProperty().divide(3.3));
+        imageView8.fitHeightProperty().bind(gameScene.heightProperty().divide(3.3));
+        Group root8 = new Group(imageView8);
+        testGrid.add(root8, 2, 1);
+
+        ImageView imageView9 = new ImageView();
+        imageView9.setImage(blank);
+        imageView9.fitWidthProperty().bind(gameScene.widthProperty().divide(3.3));
+        imageView9.fitHeightProperty().bind(gameScene.heightProperty().divide(3.3));
+        Group root9 = new Group(imageView9);
+        testGrid.add(root9, 2, 2);
+
+
+        root.setOnMouseClicked(event->{
+            if(event.getButton() == MouseButton.PRIMARY){
+                if(imageView.getImage() == blank){
+                    if (turns == 0 || turns % 2 == 0){
+                        imageView.setImage(xImage);
+                        turns ++;
+                    }else{
+                        imageView.setImage(oImage);
+                        turns += 1;
+                    }
+                    System.out.println(turns);
+                }
+
+            }
+        });
+
+        root2.setOnMouseClicked(event->{
+            if(event.getButton() == MouseButton.PRIMARY) {
+                if (imageView2.getImage() == blank) {
+                    if (turns == 0 || turns % 2 == 0) {
+                        imageView2.setImage(xImage);
+                        turns++;
+                    } else {
+                        imageView2.setImage(oImage);
+                        turns += 1;
+                    }
+                }
+            }
+        });
+
+        root3.setOnMouseClicked(event->{
+            if(event.getButton() == MouseButton.PRIMARY){
+                if(imageView3.getImage() == blank){
+                    if (turns == 0 || turns % 2 == 0){
+                        imageView3.setImage(xImage);
+                        turns ++;
+                    }else{
+                        imageView3.setImage(oImage);
+                        turns += 1;
+                    }
+                }
+            }
+        });
+
+        root4.setOnMouseClicked(event->{
+            if(imageView4.getImage() == blank){
+                if(event.getButton() == MouseButton.PRIMARY){
+                    if (turns == 0 || turns % 2 == 0){
+                        imageView4.setImage(xImage);
+                        turns ++;
+                    }else{
+                        imageView4.setImage(oImage);
+                        turns += 1;
+                    }
+                }
+            }
+        });
+
+        root5.setOnMouseClicked(event->{
+            if(imageView5.getImage() == blank){
+                if(event.getButton() == MouseButton.PRIMARY){
+                    if (turns == 0 || turns % 2 == 0){
+                        imageView5.setImage(xImage);
+                        turns ++;
+                    }else{
+                        imageView5.setImage(oImage);
+                        turns += 1;
+                    }
+                }
+            }
+        });
+
+        root6.setOnMouseClicked(event->{
+            if(imageView6.getImage() == blank){
+                if(event.getButton() == MouseButton.PRIMARY){
+                    if (turns == 0 || turns % 2 == 0){
+                        imageView6.setImage(xImage);
+                        turns ++;
+                    }else{
+                        imageView6.setImage(oImage);
+                        turns += 1;
+                    }
+                }
+            }
+        });
+
+        root7.setOnMouseClicked(event->{
+            if(imageView7.getImage() == blank){
+                if(event.getButton() == MouseButton.PRIMARY){
+                    if (turns == 0 || turns % 2 == 0){
+                        imageView7.setImage(xImage);
+                        turns ++;
+                    }else{
+                        imageView7.setImage(oImage);
+                        turns += 1;
+                    }
+                }
+            }
+        });
+
+        root8.setOnMouseClicked(event->{
+            if(imageView8.getImage() == blank){
+                if(event.getButton() == MouseButton.PRIMARY){
+                    if (turns == 0 || turns % 2 == 0){
+                        imageView8.setImage(xImage);
+                        turns ++;
+                    }else{
+                        imageView8.setImage(oImage);
+                        turns += 1;
+                    }
+                }
+            }
+        });
+
+        root9.setOnMouseClicked(event->{
+            if(imageView9.getImage() == blank){
+                if(event.getButton() == MouseButton.PRIMARY){
+                    if (turns == 0 || turns % 2 == 0){
+                        imageView9.setImage(xImage);
+                        turns ++;
+                    }else{
+                        imageView9.setImage(oImage);
+                        turns += 1;
+                    }
+                }
+            }
+        });
 
 
     }
@@ -127,5 +245,6 @@ public class GameBoard extends BorderPane {
     public Scene returnScene(){
         return gameScene;
     }
+
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,30 +3,16 @@ package sample;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.*;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.*;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 
 
 public class Main extends Application {
 
-    // initializes scenes
-    private Scene startScene;
+    private static GameBoard gameBoard;
+    private static StartBoard startBoard;
 
 
     public static void main(String[] args) {
@@ -38,13 +24,13 @@ public class Main extends Application {
         primaryStage.setTitle("Tic Tac Toe");
 
 
-        StartBoard startBoard = new StartBoard();
+        startBoard = new StartBoard();
         primaryStage.setScene(startBoard.returnScene());
 
         Button btn = startBoard.returnStart();
 
 
-        GameBoard gameBoard = new GameBoard();
+        gameBoard = new GameBoard();
 
 
         btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -54,6 +40,7 @@ public class Main extends Application {
 
             }
         });
+
 
 
 
